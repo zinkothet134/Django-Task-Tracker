60322b25405399d7fd42ab313760eeefc044b4fe
Zin Ko Thet <zinkothet153586@gmail.com>
2026-03-24 22:51:38 +0700

    Url modify

--- task_tracker_project/urls.py
+++ task_tracker_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/tasks/", permanent=False)),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("tasks/", include("tasks.urls")),
