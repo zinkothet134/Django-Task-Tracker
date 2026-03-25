bba7081104701065acab0da90ec24ba4caea2b0c
Zin Ko Thet <zinkothet153586@gmail.com>
2026-03-25 08:49:09 +0700

    removing surper suer part

--- task_tracker_project/settings.py
+++ task_tracker_project/settings.py
@@ -171,16 +171,6 @@ else:
         }
     }
 
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
